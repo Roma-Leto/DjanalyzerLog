324ee9c2ad65dc5a08d5b19b20e1a897ba2ac6c9
Roma-Leto <119625441+Roma-Leto@users.noreply.github.com>
2025-04-11 15:12:27 +0300

    Описан базовый абстрактный класс генерации отчётов

---
+++ reports/base.py
@@ -0,0 +1,46 @@
+"""
+Содержит абстрактный класс BaseReport, определяющий интерфейс для генерации отчётов на
+основе логов.
+
+Класс включает в себя:
+- process_file(): обработка одного файла
+- merge_result(): объединение результатов
+- format_result(): форматирование данных
+- generate(): полный pipeline
+"""
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Dict, List, Any
+
+
+class BaseReport(ABC):
+    """
+    Абстрактный базовый класс для всех типов отчётов
+    """
+
+    @property
+    @abstractmethod
+    def name(self):
+        """Уникальное имя отчёта используемое в CLI"""
+        pass
+
+    @abstractmethod
+    def process_file(self, file_path: Path) -> Dict[str, Any]:
+        """Обработка лога и возврат промежуточных данных"""
+        pass
+
+    @abstractmethod
+    def merge_result(self, result: List[Dict[str, Any]]) -> Dict[str, Any]:
+        """Объединение результатов из нескольких файлов в один лог"""
+        pass
+
+    @abstractmethod
+    def format_result(self, merge_data: Dict[str, Any]) -> str:
+        """Функция форматирования результата для вывода пользователю"""
+        pass
+
+    def generate(self, file_path: List[Path]) -> str:
+        """Генерация польного отчёта"""
+        result = [self.process_file(fp) for fp in file_path]
+        merged_data = self.merge_result(result)
+        return self.format_result(merged_data)
