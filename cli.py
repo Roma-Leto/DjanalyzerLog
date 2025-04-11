a05a48fa2f180ea2f889da9b8bdece6568622308
Roma-Leto <119625441+Roma-Leto@users.noreply.github.com>
2025-04-11 12:49:17 +0300

    Разбор аргументов командной строки

--- cli.py
+++ cli.py
@@ -3,12 +3,38 @@
 """
 
 import argparse
+from pathlib import Path
 from typing import List, Optional
 
 
-def parse_args():
-    pass
+def parse_args(args: Optional[List[str]] = None) -> argparse.Namespace:
+    """
+    Функция разбора аргументов командной строки.
+    :param args: Список строк
+    :return: Namespace(log_files=[WindowsPath(''), WindowsPath('')], report='')
+    """
+    parser = argparse.ArgumentParser(description="Django logs analyzer")
+
+    parser.add_argument(
+        "log_files",
+        nargs="+",
+        type=Path,
+        help="Paths to log files"
+    )
+
+    parser.add_argument(
+        "--report",
+        required=True,
+        choices=None,
+        help="Report type to generate"
+    )
+
+    return parser.parse_args(args)
 
 
 def run_cli():
-    print("ok")
+    """
+    Входная функция приложения
+    """
+    args = parse_args()
+    print(args)
